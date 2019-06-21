d7efbb007206f560a7af017cd3907d78b8f958f2
Mathew <24691566+mesporas1@users.noreply.github.com>
2019-06-20 22:24:40 -0700

    Started working on models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Topping(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Pizza(models.Model):
+    type = models.CharField(max_length=64)
+    size = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits = 10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.size} {self.type} pizza: price {self.price}"
+
+class Sub(models.Model):
+    type = models.CharField(max_length=64)
+    size = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits = 10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"{self.size} {self.type} sub: price {self.price}"
+        
+class DinnerPlatter(models.Model):
+    name = models.CharField(max_length=64)
+    size = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits = 10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.size} {self.name} dinnerPlatter: price {self.price}"
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits = 10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}: price {self.price}"
+
+class Salads(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits = 10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}: price {self.price}"
